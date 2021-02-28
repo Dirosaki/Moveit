3b37ea0dd841943b35054ad77dbe51bf812ff535
Dirosaki <diegosgomes27@gmail.com>
2021-02-28 10:02:15 -0300

    Finalização do Projeto Move.it

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,77 @@
+import { useState, useEffect, useContext } from "react";
+import { CountdownContext } from "../contexts/CountdownContext";
+import styles from "../styles/components/Countdown.module.css";
+
+export function Countdown() {
+    const {
+        minutes,
+        seconds,
+        hasFinished,
+        isActive,
+        resetCountdown,
+        startCountdown,
+    } = useContext(CountdownContext);
+
+    const [minuteLeft, minuteRight] = String(minutes)
+        .padStart(2, "0")
+        .split("");
+    const [secondLeft, secondRight] = String(seconds)
+        .padStart(2, "0")
+        .split("");
+
+    return (
+        <div>
+            <div className={styles.countdownContainer}>
+                <div>
+                    <span>{minuteLeft}</span>
+                    <span>{minuteRight}</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>{secondLeft}</span>
+                    <span>{secondRight}</span>
+                </div>
+            </div>
+
+            {hasFinished ? (
+                <button disabled className={styles.countdownButton}>
+                    Ciclo encerrado
+                    <img src="icons/finished.svg" alt="Finished" />
+                </button>
+            ) : (
+                <>
+                    {isActive ? (
+                        <button
+                            type="button"
+                            className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                            onClick={resetCountdown}
+                        >
+                            Abandonar ciclo
+                            <svg
+                                width="14"
+                                height="14"
+                                viewBox="0 0 14 14"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z"
+                                    fill="#666666"
+                                />
+                            </svg>
+                        </button>
+                    ) : (
+                        <button
+                            type="button"
+                            className={styles.countdownButton}
+                            onClick={startCountdown}
+                        >
+                            Iniciar um ciclo
+                            <img src="icons/start.svg" alt="Start" />
+                        </button>
+                    )}
+                </>
+            )}
+        </div>
+    );
+}
